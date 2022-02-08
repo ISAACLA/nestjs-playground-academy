9125ed90db8726780924b1e4382b76fc098537fe
ISAACLA <yanbinla@gmail.com>
2022-02-07 23:41:10 -0800

    added contacts module

---
+++ src/contacts/entities/contact.entity.ts
@@ -0,0 +1,33 @@
+import { Column, Entity, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { BaseEntity } from '../../base-entity';
+import { Institute } from '../../institutes/entities/institute.entity';
+
+@Entity()
+export class Contact extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  phone: string;
+
+  @Column()
+  address1: string;
+
+  @Column()
+  address2: string;
+
+  @Column()
+  state: string;
+
+  @Column()
+  zip: string;
+
+  @Column()
+  email: string;
+
+  /*
+   * This is un-directional, uncomment the following can make it bi-directional;
+   * */
+  @OneToOne(() => Institute, (institute) => institute.contact)
+  institute: Institute;
+}
